74260a048745e83705403a4a4f4c059cfe0a13cf
Marcus P <fake@fakemail.com>
2025-01-26 20:02:27 -0800

    started template

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", test)
+	mux.HandleFunc("/test", index)
 
 	log.Println("Server listening on port: 8080")
 	http.ListenAndServe(":8080", mux)
@@ -17,4 +19,12 @@ func main() {
 func test(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("test"))
 	log.Println("hit handler")
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("index.html").ParseFiles("index.html")
+	if err != nil {
+		log.Fatal("template error")
+	}
+	err = t.Execute(w, "test")
 }
